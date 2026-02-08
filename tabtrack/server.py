18f867b0001aec477651d488ce6e288dd9488fb5
Dmitry Gritsenko <git@evtn.me>
2026-02-08 20:06:13 +0400

    fix: cors

--- tabtrack/server.py
+++ tabtrack/server.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from tabtrack.database import init_db
 from tabtrack.routes import collect, keys, push, sse
@@ -25,3 +26,15 @@ app.include_router(collect.router)
 app.include_router(keys.keys_router)
 app.include_router(keys.admin_router)
 app.include_router(sse.router)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5137",
+        "https://tabs.evtn.me",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
